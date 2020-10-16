5b50f76de4c4063af08d4e18e845c950508fbda9
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2020-10-16 16:44:42 -0500

    Creación de funcionalidad de pantalla de inicio
    Se creó el splash con el logotipo en la pantalla de inicio del sistema

---
+++ Android/app/src/main/java/com/example/geoofertas/Inicio.java
@@ -0,0 +1,28 @@
+package com.example.geoofertas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Inicio extends AppCompatActivity {
+
+    ImageView logo, splashImg;
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        logo = findViewById(R.id.imageLogo);
+        splashImg = findViewById(R.id.imagePrincipal);
+        lottieAnimationView = findViewById(R.id.robot);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+}
