aa46d79684cd9e46ae620980a8212019fbfad558
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2020-10-17 03:09:41 -0500

    Creación de ventana inicio de sesión y registro

--- Android/app/src/main/java/com/example/geoofertas/Inicio.java
+++ Android/app/src/main/java/com/example/geoofertas/Inicio.java
@@ -2,12 +2,16 @@ package com.example.geoofertas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
-public class Inicio extends AppCompatActivity {
+public class Inicio extends AppCompatActivity implements Animator.AnimatorListener {
 
     ImageView logo, splashImg;
     LottieAnimationView lottieAnimationView;
@@ -24,5 +28,27 @@ public class Inicio extends AppCompatActivity {
         splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
         logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+        splashImg.addAnimatorListener(this);
     }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        Intent intento = new Intent(this, InicioSesion.class);
+        startActivity(intento);
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+        
 }
---
+++ Android/app/src/main/java/com/example/geoofertas/InicioSesion.java
@@ -0,0 +1,36 @@
+package com.example.geoofertas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class InicioSesion extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Inicio sesión"));
+        tabLayout.addTab(tabLayout.newTab().setText("Registrarme"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
