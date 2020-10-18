362a96ebaef7a6f7ccb2dc030e8383958e4ca5aa
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2020-10-18 05:13:13 -0500

    Creación de ventana Todas las ofertas

---
+++ Android/app/src/main/java/com/example/geoofertas/TodasLasOfertas.java
@@ -0,0 +1,14 @@
+package com.example.geoofertas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodasLasOfertas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todas_las_ofertas);
+    }
+}
