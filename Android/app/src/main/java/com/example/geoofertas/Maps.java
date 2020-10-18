b48bdb05a46041b32b42a18f0e350b2138fc86e0
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-10-18 00:17:12 -0500

    Se agrega la funcionalidad parcial del mapview

--- Android/app/src/main/java/com/example/geoofertas/Maps.java
+++ Android/app/src/main/java/com/example/geoofertas/Maps.java
@@ -1,17 +1,37 @@
 package com.example.geoofertas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Maps extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import static com.example.geoofertas.util.Constants.MAPVIEW_BUNDLE_KEY;
+
+public class Maps extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Bundle mapViewBlunde = null;
+        if(savedInstanceState != null){
+            mapViewBlunde = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(mapViewBlunde);
+        mMapView.getMapAsync(this);
     }
 
     @Override
@@ -19,5 +39,66 @@ public class Maps extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.searchview, menu);
         MenuItem menuItem = menu.findItem(R.id.searchIcon);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        //googleMap.moveCamera();
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
